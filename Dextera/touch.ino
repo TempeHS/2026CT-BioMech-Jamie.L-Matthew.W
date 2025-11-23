b0d5c2f24c9ef47685ab34164a6c86b4907a50e7
Matthew W <matthew.wu12@education.nsw.gov.au>
2025-11-24 09:35:24 +1100

    a

--- Dextera/touch.ino
+++ Dextera/touch.ino
@@ -4,7 +4,24 @@
 Adafruit_MPR121.h touchSensor = Adafruit_MPR121.h();
 
 void touch_setup() {
-  serial.begin(9600);
+  Serial.begin(9600);
+  while (!Serial) { delay(10); }
+
+  Serial.println("MRP121 touch sensor test")
   wire.begin();
-    if (!)
+
+  if (!cap.begin(0x5A)) {
+    Serial.println("Error: Yo shi not working.");
+    Serial.println("Run I2C scanner if needed.");
+    while (true) delay (1000);
+  }
+  Serial.println("Yo shi is working");
+}
+
+uint16_t lastTouched = 0;
+
+void touch_loop() {
+  uint16_t touched = cap.touched();
+
+  if (touched)
 }
