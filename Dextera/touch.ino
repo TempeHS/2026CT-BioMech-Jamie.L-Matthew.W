2cf262e707337d7542dccd450656e5efd40e5e00
Matthew W <matthew.wu12@education.nsw.gov.au>
2025-12-09 22:37:26 +0000

    dadas

--- Dextera/touch.ino
+++ Dextera/touch.ino
@@ -3,17 +3,22 @@
 
 Adafruit_MPR121 cap;
 
-void touch_setup() {
+void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  cap.begin();
+  Wire.begin();
+  if (!cap.begin(0x5A)) {  // check init (0x5A is common MPR121 address)
+    Serial.println("MPR121 not found, check wiring");
+    while (1)
+      delay(1000);
+  }
   Serial.println("stuff is initialized");
 }
 
 uint16_t lastTouched = 0;
 
-void touch_loop(); {
+void loop() {
   uint16_t touched = cap.touched();
 
   if (touched != lastTouched) {
@@ -21,13 +26,17 @@ void touch_loop(); {
     bool any = false;
     for (uint8_t i = 0; i < 12; i++) {
       if (touched & (1 << i)) {
-        if (any) Serial.print(", ");
+        if (any)
+          Serial.print(", ");
         Serial.print(i);
         any = true;
       }
     }
-    if (!any) Serial.print("none");
-    Serial.println("stop touching me");
+    if (!any)
+      Serial.print("none");
+    Serial.println(" stop touching me");
     lastTouched = touched;
   }
+
+  delay(100);
 }
